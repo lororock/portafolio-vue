091c32979a4c05ab501a7355f46bc10612736367
lororock <cristhianrosas159@hotmail.com>
2023-04-25 04:00:59 -0500

    slider proyectos

---
+++ src/assets/proyectos.js
@@ -0,0 +1,58 @@
+export const proyectos = [
+  {
+    nombre: "Inventas",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida velit at massa dignissim suscipit.",
+    imagen: "src/assets/img/inventas.png",
+    tecnologias: "Vue, Vuetify, Firebase",
+    enlace: "https://mercamejor.inventas-app.com/",
+  },
+  {
+    nombre: "Anterior portafolio",
+    descripcion:
+      "Vestibulum tincidunt purus ut justo dapibus, eget sagittis urna ultrices. Sed tincidunt enim a velit finibus, vel scelerisque tortor porttitor.",
+    imagen: "src/assets/img/oldPortafolio.png",
+    tecnologias: "HTML, CSS, JavaScript",
+    enlace: "https://cristhian-rosas.netlify.app/",
+  },
+  {
+    nombre: "DNA music",
+    descripcion:
+      "Pellentesque auctor, sapien sit amet euismod rhoncus, eros nunc faucibus mi, in maximus ex sapien at nisi.",
+    imagen: "src/assets/img/DNA.png",
+    tecnologias: "Vue, Bootstrap, Microservicios, axios",
+    enlace: "https://dnamusic.com.co/sign-in",
+  },
+  {
+    nombre: "Gestor Estudiante/Profesor",
+    descripcion:
+      "Vestibulum tincidunt purus ut justo dapibus, eget sagittis urna ultrices. Sed tincidunt enim a velit finibus, vel scelerisque tortor porttitor.",
+    imagen: "src/assets/img/login.png",
+    tecnologias: "Python, Django, PostgreSQL",
+    enlace: "https://prueba-tecnica-cristhian.netlify.app",
+  },
+  {
+    nombre: "Gestor de gastos",
+    descripcion:
+      "Pellentesque auctor, sapien sit amet euismod rhoncus, eros nunc faucibus mi, in maximus ex sapien at nisi.",
+    imagen: "src/assets/img/gastos.png",
+    tecnologias: "HTML, CSS, JavaScript",
+    enlace: "https://regal-crepe-84ee70.netlify.app/",
+  },
+  {
+    nombre: "Api peliculas",
+    descripcion:
+      "Pellentesque auctor, sapien sit amet euismod rhoncus, eros nunc faucibus mi, in maximus ex sapien at nisi.",
+    imagen: "src/assets/img/peliculas.png",
+    tecnologias: "HTML, CSS, JavaScript, Api Fetch",
+    enlace: "https://jade-seahorse-cb70ae.netlify.app/",
+  },
+  {
+    nombre: "landing page inventas",
+    descripcion:
+      "Pellentesque auctor, sapien sit amet euismod rhoncus, eros nunc faucibus mi, in maximus ex sapien at nisi.",
+    imagen: "src/assets/img/inventasWeb.png",
+    tecnologias: "HTML, Bootstrap, JavaScript",
+    enlace: "https://inventas-app.com/",
+  },
+];
--- src/components/Portafolio.vue
+++ src/components/Portafolio.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { computed, ref } from "vue";
+import { proyectos } from "../assets/proyectos";
+
+const postXpage = 4;
+const start = ref(0);
+const end = ref(4);
+const paginaActual = ref(0);
+
+const next = () => {
+  start.value = start.value + postXpage;
+  end.value = end.value + postXpage;
+  paginaActual.value = paginaActual.value + 1;
+};
+const prev = () => {
+  start.value = start.value - postXpage;
+  end.value = end.value - postXpage;
+  paginaActual.value = paginaActual.value - 1;
+};
+
+defineExpose({ proyectos });
+
+const maxLength = computed(() => proyectos.length);
+</script>
 <template>
   <section>
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -6,116 +30,55 @@
           Portafolio de proyectos
         </h2>
         <p class="mt-4 max-w-xl text-gray-500">
-            Aquí en mi portafolio, encontrarás una selección de algunos de mis proyectos más recientes. Cada uno de ellos representa un desafío único que tuve que superar y una oportunidad para demostrar mis habilidades y creatividad. Espero que disfrutes viendo mis proyectos tanto como yo disfruté trabajando en ellos.
+          Aquí en mi portafolio, encontrarás una selección de algunos de mis
+          proyectos más recientes. Cada uno de ellos representa un desafío único
+          que tuve que superar y una oportunidad para demostrar mis habilidades
+          y creatividad. Espero que disfrutes viendo mis proyectos tanto como yo
+          disfruté trabajando en ellos.
         </p>
       </header>
 
       <div class="mt-8">
-        <p class="text-sm text-gray-500">Showing <span> 4 </span> of 40</p>
+        <p class="text-sm text-gray-500">
+          mostrando
+          <span> {{ end > proyectos.length ? proyectos.length : end }} </span>
+          de {{ proyectos.length }}
+        </p>
       </div>
 
       <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <li>
-          <a href="#" class="group block overflow-hidden">
+        <li v-for="proyecto in proyectos.slice(start, end)" :key="i">
+          <div class="group block overflow-hidden">
             <img
-              src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              :src="proyecto.imagen"
               alt=""
               class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
             />
 
             <div class="relative bg-white pt-3">
-              <h3
+              <a
                 class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                target="_blank"
+                :href="proyecto.enlace"
               >
-                Basic Tee
-              </h3>
+                {{ proyecto.nombre }}
+              </a>
 
               <p class="mt-2">
-                <span class="sr-only"> Regular Price </span>
-
-                <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+                <span class="tracking-wider text-gray-900">
+                  {{ proyecto.tecnologias }}
+                </span>
               </p>
             </div>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" class="group block overflow-hidden">
-            <img
-              src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              alt=""
-              class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-            />
-
-            <div class="relative bg-white pt-3">
-              <h3
-                class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-              >
-                Basic Tee
-              </h3>
-
-              <p class="mt-2">
-                <span class="sr-only"> Regular Price </span>
-
-                <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" class="group block overflow-hidden">
-            <img
-              src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              alt=""
-              class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-            />
-
-            <div class="relative bg-white pt-3">
-              <h3
-                class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-              >
-                Basic Tee
-              </h3>
-
-              <p class="mt-2">
-                <span class="sr-only"> Regular Price </span>
-
-                <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" class="group block overflow-hidden">
-            <img
-              src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              alt=""
-              class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-            />
-
-            <div class="relative bg-white pt-3">
-              <h3
-                class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-              >
-                Basic Tee
-              </h3>
-
-              <p class="mt-2">
-                <span class="sr-only"> Regular Price </span>
-
-                <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
+          </div>
         </li>
       </ul>
 
       <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
         <li>
-          <a
-            href="#"
+          <button
+            @click="prev()"
+            :disabled="start <= 0"
             class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100"
           >
             <span class="sr-only">Prev Page</span>
@@ -131,45 +94,21 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </button>
         </li>
 
         <li>
           <a
-            href="#"
-            class="block h-8 w-8 rounded border border-gray-100 text-center leading-8"
+            class="block h-8 w-16 rounded border border-gray-100 text-center leading-8"
           >
-            1
-          </a>
-        </li>
-
-        <li
-          class="block h-8 w-8 rounded border-black bg-black text-center leading-8 text-white"
-        >
-          2
-        </li>
-
-        <li>
-          <a
-            href="#"
-            class="block h-8 w-8 rounded border border-gray-100 text-center leading-8"
-          >
-            3
+            Página {{ paginaActual + 1 }}
           </a>
         </li>
 
         <li>
-          <a
-            href="#"
-            class="block h-8 w-8 rounded border border-gray-100 text-center leading-8"
-          >
-            4
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
+          <button
+            @click="next()"
+            :disabled="end >= maxLength"
             class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100"
           >
             <span class="sr-only">Next Page</span>
@@ -185,7 +124,7 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </button>
         </li>
       </ol>
     </div>
