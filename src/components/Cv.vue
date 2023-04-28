e197667ae97cab749bfb5d3323979f5506b3c35f
lororock <cristhianrosas159@hotmail.com>
2023-04-28 04:52:18 -0500

    src in href of pdf

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -13,7 +13,7 @@
         </p>
 
         <a
-          href="../assets/document/cv-Cristhian-Rosas.pdf"
+          href="src/assets/document/cv-Cristhian-Rosas.pdf"
           download
           class="inline-block px-12 py-3 mt-8 text-sm font-medium text-indigo-600 border border-indigo-600 rounded-full hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
         >
