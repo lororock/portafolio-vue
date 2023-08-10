cb5f2670f7fa2e89bfa4acfe88684c6ae714c611
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-08-10 00:15:05 -0500

    delete defineEmits import

--- src/components/Correo.vue
+++ src/components/Correo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineEmits } from "vue";
+import { ref } from "vue";
 import emailjs from "emailjs-com";
 import Swal from "sweetalert2";
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineEmits } from "vue";
+import { ref } from "vue";
 
 const mostrar = ref(false);
 const mostrar2 = ref(false);
