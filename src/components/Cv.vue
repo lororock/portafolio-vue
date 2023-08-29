d21b83eb934e29656bcb8cfa5945a70984683338
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-08-28 19:51:44 -0500

    upDate Cv

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -13,7 +13,7 @@
         </p>
 
         <a
-          href="https://drive.google.com/file/d/1LDBbVJBPXi1bao8bew9u_QSOY0AeiOoT/view?usp=sharing"
+          href="https://drive.google.com/file/d/18Ex5uNZsV4el_P_nZa-1-eiXfAyEd5O4/view?usp=sharing"
           target="_blank"
           class="inline-block px-12 py-3 mt-8 text-sm font-medium text-indigo-600 border border-indigo-600 rounded-full hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
         >
