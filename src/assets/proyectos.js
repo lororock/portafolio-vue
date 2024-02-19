a89f7df9df13eb0cae3a5017184f68ec86f39a54
Cristhian Rosas <cristhianrosas159@hotmail.com>
2024-02-19 16:43:32 -0500

    dna link update

--- src/assets/proyectos.js
+++ src/assets/proyectos.js
@@ -48,7 +48,7 @@ export const proyectos = [
     imagen: dna,
     proyecto: fullDna,
     tecnologias: "Vue, Bootstrap, Microservicios, axios",
-    enlace: "https://dnamusic.com.co/sign-in",
+    enlace: "https://dnamusic.edu.co/portal-estudiantes",
   },
   {
     nombre: "Aythen",
