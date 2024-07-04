6c450c1381468a0e65c2b4c7dfd81003f224238e
Cristhian Rosas <cristhianrosas159@hotmail.com>
2024-07-04 04:49:57 -0500

    cv update

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -10,7 +10,7 @@
           Personaliza tus productos según tus necesidades y hazlos únicos.
         </p>
 
-        <a href="https://drive.google.com/file/d/19k6dPvuvz_ChTgX446dyeJbjBm61ySRM/view?usp=drive_link" target="_blank"
+        <a href="https://drive.google.com/file/d/1GWsZCxx1kUCtdnxsQeAzf-4_a7zGppab/view" target="_blank"
           class="inline-block px-12 py-3 mt-8 text-sm font-medium text-indigo-600 border border-indigo-600 rounded-full hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500">
           Descargar mi CV
         </a>
