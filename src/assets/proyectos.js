6dfb3deca5787c5eb6d59ff94f942127b5fc07a6
Cristhian Rosas <cristhianrosas159@hotmail.com>
2024-07-20 07:00:59 -0500

    update img from porfolio

--- src/assets/proyectos.js
+++ src/assets/proyectos.js
@@ -1,25 +1,17 @@
 import inventas from "../assets/img/principal/inventas.webp";
 import portafolio from "../assets/img/principal/oldportafolio.webp";
 import dna from "../assets/img/principal/DNA.webp";
-import estudianteProfesor from "../assets/img/principal/login.webp";
 import presupuesto from "../assets/img/principal/gastos.webp";
 import peliculas from "../assets/img/principal/peliculas.webp";
-import landing from "../assets/img/principal/landing.webp";
 import aythen from "../assets/img/principal/Aythen.webp";
-import enLinea from "../assets/img/principal/tres-en-linea.webp";
-import doubleVpartners from "../assets/img/principal/double-v-partners-prueba-tecnica.webp";
 import dermocapilar from "../assets/img/principal/dermocapilar.webp";
 
 import fullInventas from "../assets/img/fullscreen/inventas.webp";
 import fullPortafolio from "../assets/img/fullscreen/portafolio.webp";
 import fullDna from "../assets/img/fullscreen/DNA.webp";
-import fullEstudianteProfesor from "../assets/img/fullscreen/prueba.webp";
 import fullPresupuesto from "../assets/img/fullscreen/gastos.webp";
 import fullPeliculas from "../assets/img/fullscreen/peliculas.webp";
-import fullLanding from "../assets/img/fullscreen/landing.webp";
 import fullAythen from "../assets/img/fullscreen/Aythen.webp";
-import fullTresEnLinea from "../assets/img/fullscreen/linea.webp";
-import fullDoubleVpartners from "../assets/img/fullscreen/double-v-partners-prueba-tecnica.webp";
 import fullDermocapilar from "../assets/img/fullscreen/dermocapilar.webp";
 
 export const proyectos = [
@@ -69,31 +61,13 @@ export const proyectos = [
     enlace: "https://cristhian-rosas.netlify.app",
   },
   {
-    nombre: "Gestor Estudiante/Profesor",
+    nombre: "Api peliculas",
     descripcion:
-      "Inicio de sesión y registro que gestionan a dos tipos de usuarios, cada uno con su propia interfaz y funcionalidades personalizadas, según su rol en el sistema. Lamentablemente, el backend se encuentra temporalmente fuera de servicio.",
-    imagen: estudianteProfesor,
-    proyecto: fullEstudianteProfesor,
-    tecnologias: "Vue, Bootstrap, axios",
-    enlace: "https://prueba-tecnica-cristhian.netlify.app",
-  },
-  {
-    nombre: "Prueba técnica Double V Partners",
-    descripcion:
-      "Prueba técnica para Double V Partners en la cual no se permiten frameworks de diseño como Tailwind o Bootstrap, solo Sass y Scss.",
-    imagen: doubleVpartners,
-    proyecto: fullDoubleVpartners,
-    tecnologias: "Vue, Sass, Axios",
+      "Consumo de API para la creación de una página que muestra las últimas películas con su nombre y portada. Además, incluye paginación para navegar hasta películas más antiguas.",
+    imagen: peliculas,
+    proyecto: fullPeliculas,
+    tecnologias: "Vue, Css, Axios",
     enlace: "https://double-v-partners-prueba-tecnica.netlify.app",
-  },
-  {
-    nombre: "Tres En Linea",
-    descripcion:
-      "Es una implementación digital del clásico juego de estrategia en el que dos jugadores compiten para lograr una línea de tres símbolos iguales en un tablero de 3x3. Esta versión digital se desarrollará utilizando tecnologías web modernas y estará diseñada para proporcionar una experiencia de juego interactiva y atractiva.",
-    imagen: enLinea,
-    proyecto: fullTresEnLinea,
-    tecnologias: "React, Bootstrap, Sweetalert2",
-    enlace: "https://phenomenal-bunny-tres-en-linea.netlify.app",
   },
   {
     nombre: "Gestor de gastos",
@@ -103,23 +77,5 @@ export const proyectos = [
     proyecto: fullPresupuesto,
     tecnologias: "HTML, CSS, JavaScript",
     enlace: "https://regal-crepe-84ee70.netlify.app",
-  },
-  {
-    nombre: "Api peliculas",
-    descripcion:
-      "Consumo de API para la creación de una página que muestra las últimas películas con su nombre y portada. Además, incluye paginación para navegar hasta películas más antiguas.",
-    imagen: peliculas,
-    proyecto: fullPeliculas,
-    tecnologias: "HTML, CSS, JavaScript, Api Fetch",
-    enlace: "https://jade-seahorse-cb70ae.netlify.app",
-  },
-  {
-    nombre: "Landing page inventas",
-    descripcion:
-      "Mi primera página web empresarial para mostrar un equipo de desarrollo y sus servicios, con información de contacto y presentación de los integrantes del equipo.",
-    imagen: landing,
-    proyecto: fullLanding,
-    tecnologias: "HTML, Bootstrap, JavaScript",
-    enlace: "https://inventas-app.com",
   },
 ];
--- src/components/Portafolio.vue
+++ src/components/Portafolio.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import { proyectos } from "../assets/proyectos";
 import Swal from "sweetalert2";
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -9,33 +9,10 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
-// Setup for pagination
-const postXpage = 4;
-const start = ref(0);
-const end = ref(postXpage);
-const paginaActual = ref(0);
-
-// Pagination methods
-const next = () => {
-  if (end.value < proyectos.length) {
-    start.value += postXpage;
-    end.value += postXpage;
-    paginaActual.value += 1;
-  }
-};
-const prev = () => {
-  if (start.value > 0) {
-    start.value -= postXpage;
-    end.value -= postXpage;
-    paginaActual.value -= 1;
-  }
-};
-
-// SweetAlert method
 const mostrarPopAds = (pro) => {
-  console.log(pro);
+  console.log("el prop", pro);
   Swal.fire({
-    text: pro.descripcion,
+    title: pro.descripcion,
     imageUrl: pro.proyecto,
     width: 1200,
     padding: "3em",
@@ -57,12 +34,6 @@ const mostrarPopAds = (pro) => {
   });
 };
 
-// Exposing projects
-defineExpose({ proyectos });
-
-// Computed property for total projects length
-const maxLength = computed(() => proyectos.length);
-
 // Swiper methods and modules
 const onSwiper = (swiper) => {
   console.log(swiper);
@@ -71,6 +42,10 @@ const onSlideChange = () => {
   console.log('slide change');
 };
 const modules = ref([Navigation, Pagination, Scrollbar]);
+
+// Exposing projects
+defineExpose({ proyectos });
+
 </script>
 
 
@@ -91,80 +66,16 @@ const modules = ref([Navigation, Pagination, Scrollbar]);
       </header>
 
       <div class="mt-8 mb-24">
-        <swiper class="bg-slate-200 rounded-lg border border-gray-200 shadow-lg p-4" :modules="modules" :slides-per-view="1" 
-          :space-between="50" navigation :pagination="{ dynamicBullets: true }" @swiper="onSwiper"
+        <swiper class="bg-slate-200 rounded-lg border border-gray-200 shadow-lg p-4" :modules="modules"
+          :slides-per-view="1" :space-between="50" navigation :pagination="{ dynamicBullets: true }" @swiper="onSwiper"
           @slideChange="onSlideChange">
           <swiper-slide v-for="(pro, index) in proyectos" :key="index">
             <div @click="mostrarPopAds(pro)">
-              <img :src="pro.proyecto" :alt="pro.descripcion" />
+              <img class="mh-full" :src="pro.imagen" :alt="pro.descripcion" />
             </div>
           </swiper-slide>
         </swiper>
       </div>
-
-
-      <!--       
-      <div class="mt-8">
-        <p class="text-sm text-gray-500">
-          mostrando
-          <span> {{ end > proyectos.length ? proyectos.length : end }} </span>
-          de {{ proyectos.length }}
-        </p>
-      </div>
-
-      <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <li v-for="proyecto in proyectos.slice(start, end)" :key="i">
-          <div class="group block overflow-hidden">
-            <img @click="mostrarPopAds(proyecto)" :src="proyecto.imagen" alt=""
-              class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]" />
-
-            <div class="relative bg-white pt-3">
-              <a class="text-x font-semibold text-indigo-600 group-hover:underline group-hover:underline-offset-4"
-                target="_blank" :href="proyecto.enlace">
-                {{ proyecto.nombre }}
-              </a>
-
-              <p class="mt-2">
-                <span class="tracking-wider text-gray-900">
-                  {{ proyecto.tecnologias }}
-                </span>
-              </p>
-            </div>
-          </div>
-        </li>
-      </ul>
-
-      <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
-        <li>
-          <button @click="prev()" :disabled="start <= 0"
-            class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100">
-            <span class="sr-only">Prev Page</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd" />
-            </svg>
-          </button>
-        </li>
-
-        <li>
-          <a class="block h-8 w-16 rounded border border-gray-100 text-center leading-8">
-            Página {{ paginaActual + 1 }}
-          </a>
-        </li>
-
-        <li>
-          <button @click="next()" :disabled="end >= maxLength"
-            class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100">
-            <span class="sr-only">Next Page</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd" />
-            </svg>
-          </button>
-        </li>
-      </ol> -->
     </div>
   </section>
 </template>
